f8f30005e627fb61390a9b2585620c292e0b8ecb
Kauk777 <curokky777@gmail.com>
2020-09-28 16:27:30 +0530

    [KAUSHAL]Added: UC1 validation first mandatory part

---
+++ src/com/bridgelabz/emailpattern/EmailPattern.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.emailpattern;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class EmailPattern {
+	//Checking first Mandatory part before @
+	private static final Pattern EMAIL_PATTERN=Pattern.compile("^[0-9a-zA-Z_+-]+(\\.?[0-9a-zA-Z_+-]*)@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]+)$");
+	
+	public boolean emailValidate(String email ) {
+		return EMAIL_PATTERN.matcher(email).matches();
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		EmailPattern emailCheck=new EmailPattern();
+		System.out.println("Enter the email id: ");
+		String email=sc.next();
+		System.out.println("Email Validation: "+emailCheck.emailValidate(email));
+	}
+
+}
