b662e8519f88a4e4c098313e266dbf97abb3429d
Kauk777 <curokky777@gmail.com>
2020-09-28 18:00:07 +0530

    [KAUSHAL]Added: UC5 final email validation

--- src/com/bridgelabz/emailpattern/EmailPattern.java
+++ src/com/bridgelabz/emailpattern/EmailPattern.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class EmailPattern {
 	//Checking first Mandatory part before @
-	private static final Pattern EMAIL_PATTERN=Pattern.compile("^[0-9a-zA-Z_+-]+(\\.?[0-9a-zA-Z_+-]*)[^\\.]@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]{2,})$");
+	private static final Pattern EMAIL_PATTERN=Pattern.compile("^[0-9a-zA-Z_+-]+(\\.?[0-9a-zA-Z_+-]*)[^\\.]@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]{2,})(\\.?[a-zA-Z]{2,})?$");
 	
 	public boolean emailValidate(String email ) {
 		return EMAIL_PATTERN.matcher(email).matches();
@@ -14,9 +14,38 @@ public class EmailPattern {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		EmailPattern emailCheck=new EmailPattern();
-		System.out.println("Enter the email id with optional element: ");
+		System.out.println("*****SAMPLE EMAILS TO TEST*****");
+		System.out.println("**VALID EMAILS**");
+		System.out.println("Email Validation abc@yahoo.com: "+emailCheck.emailValidate("abc@yahoo.com"));
+		System.out.println("Email Validation abc-100@yahoo.com: "+emailCheck.emailValidate("abc-100@yahoo.com"));
+		System.out.println("Email Validation abc.100@yahoo.com: "+emailCheck.emailValidate("abc.100@yahoo.com"));
+		System.out.println("Email Validation abc111@yabc.com: "+emailCheck.emailValidate("abc111@abc.com"));
+		System.out.println("Email Validation abc-100@abc.net: "+emailCheck.emailValidate("abc-100@abc.net"));
+		System.out.println("Email Validation abc.100@abc.com.au: "+emailCheck.emailValidate("abc.100@abc.com.au"));
+		System.out.println("Email Validation abc@1.com: "+emailCheck.emailValidate("abc@1.com"));
+		System.out.println("Email Validation abc@gmail.com.com: "+emailCheck.emailValidate("abc@gmail.com.com"));
+		System.out.println("Email Validation abc+100@gmail.com: "+emailCheck.emailValidate("abc+100@gmail.com"));
+		System.out.println("**INVALID EMAILS**");
+		System.out.println("Email Validation abc: "+emailCheck.emailValidate("abc"));
+		System.out.println("Email Validation abc@.com.my: "+emailCheck.emailValidate("abc@.com.my"));
+		System.out.println("Email Validation abc123@gmail.a: "+emailCheck.emailValidate("abc123@gmail.a"));
+		System.out.println("Email Validation abc123@.com: "+emailCheck.emailValidate("abc123@.com"));
+		System.out.println("Email Validation abc123@.com.com: "+emailCheck.emailValidate("abc123@.com.com"));
+		System.out.println("Email Validation .abc@abc.com: "+emailCheck.emailValidate(".abc@abc.com"));
+		System.out.println("Email Validation abc()*@gmail.com: "+emailCheck.emailValidate("abc()*@gmail.com"));
+		System.out.println("Email Validation abc@%*.com: "+emailCheck.emailValidate("abc@%*.com"));
+		System.out.println("Email Validation abc..2002@gmail.com: "+emailCheck.emailValidate("abc..2002@gmail.com"));
+		System.out.println("Email Validation abc.@gmail.com: "+emailCheck.emailValidate("abc.@gmail.com"));
+		System.out.println("Email Validation abc@abc@gmail.com: "+emailCheck.emailValidate("abc@abc@gmail.com"));
+		System.out.println("Email Validation abc@gmail.com.1a: "+emailCheck.emailValidate("abc@gmail.com.1a"));
+		System.out.println("Email Validation abc@gmail.com.aa.au: "+emailCheck.emailValidate("abc@gmail.com.aa.au"));
+		System.out.println("Enter the email id to check: ");
 		String email=sc.next();
 		System.out.println("Email Validation: "+emailCheck.emailValidate(email));
+		
+		
+		
+		
 	}
 
 }
